4df4bd4e759a58a8375e7c59a085e6c1df4ec7d8
SymbolixAU <dcooley@symbolix.com.au>
2019-05-17 11:08:32 +1000

    rebasing

--- src/mongolite.h
+++ src/mongolite.h
@@ -1,6 +1,9 @@
+#define R_NO_REMAP
+#define STRICT_R_HEADERS
+
 #include <Rinternals.h>
-#include <bson.h>
-#include <mongoc.h>
+#include <bson/bson.h>
+#include <mongoc/mongoc.h>
 
 #define stop(...) Rf_errorcall(R_NilValue, __VA_ARGS__)
 
